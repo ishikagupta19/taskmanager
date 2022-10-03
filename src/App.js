4c3d95376011ee736c236d700853ec9cb550444b
ishikagupta19 <ishika2001ishika@gmail.com>
2022-10-03 18:30:51 +0530

    update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Navb/>
       <Dashboard/>
       <Post/>
+      <Post/>
     </div>
   );
 }
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -28,18 +28,16 @@ const Post = () => {
                 </div>
                 <img src="https://i.imgur.com/UXdKE3o.jpg" className="post-image" alt=""/>
                 <div className="post-content">
-                    <div className="reaction-wrapper">
-                        <img src="img/like.PNG" className="icon" alt=""/>
-                        <img src="img/comment.PNG" className="icon" alt=""/>
-                        <img src="img/send.PNG" className="icon" alt=""/>
-                        <img src="img/save.PNG" className="save icon" alt=""/>
-                        <div class="d-flex justify-content-between align-items-center">
+                    
+                <div class="d-flex justify-content-between align-items-center">
                 <div
                   onClick={() => setliked(!liked)}
                   style={{ cursor: "pointer" }}
                   class="d-flex flex-row icons d-flex align-items-center"
                 >
+                
                   {liked ? <ThumbUpIcon /> : <ThumbUpOffAltIcon />}
+                  
                 </div>
                 <div
                   onClick={() => alert("Commented!")}
@@ -55,11 +53,10 @@ const Post = () => {
                 >
                   {saved ? <TurnedInIcon /> : <TurnedInNotIcon />}
                 </div>
-              </div>
+             
                     </div>
-                    <p className="likes">1,012 likes</p>
-                    <p className="description"><span>username </span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur tenetur veritatis placeat, molestiae impedit aut provident eum quo natus molestias?</p>
-                    <p className="post-time">2 minutes ago</p>
+                    
+                    
                 </div>
                 <div className="comment-wrapper">
                     <img src="img/smile.PNG" className="icon" alt=""/>
@@ -67,32 +64,7 @@ const Post = () => {
                     <button className="comment-btn">post</button>
                 </div>
             </div>
-            <div className="post">
-                <div className="info">
-                    <div className="user">
-                        <div className="profile-pic"><img src="img/cover 2.png" alt=""/></div>
-                        <p className="username">modern_web_channel</p>
-                    </div>
-                    <img src="img/option.PNG" className="options" alt=""/>
-                </div>
-                <img src="img/cover 2.png" className="post-image" alt=""/>
-                <div className="post-content">
-                    <div className="reaction-wrapper">
-                        <img src="img/like.PNG" className="icon" alt=""/>
-                        <img src="img/comment.PNG" className="icon" alt=""/>
-                        <img src="img/send.PNG" className="icon" alt=""/>
-                        <img src="img/save.PNG" className="save icon" alt=""/>
-                    </div>
-                    <p className="likes">1,012 likes</p>
-                    <p className="description"><span>username </span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur tenetur veritatis placeat, molestiae impedit aut provident eum quo natus molestias?</p>
-                    <p className="post-time">2 minutes ago</p>
-                </div>
-                <div className="comment-wrapper">
-                    <img src="img/smile.PNG" className="icon" alt=""/>
-                    <input type="text" className="comment-box" placeholder="Add a comment"/>
-                    <button className="comment-btn">post</button>
-                </div>
-            </div>
+           
            
         </div>
     </div>
